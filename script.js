ca0ed15af477ac2a2fbeb3ebe4a95bfdf2c253da
MonicaOspina <monipoc@gmail.com>
2017-11-15 12:14:42 -0500

    configuring jest

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export function generateRandomNumber(){
+export function generateRandomColor(){
         return {r:Math.floor(Math.random() * 256), 
         g:Math.floor(Math.random() * 256), b: Math.floor(Math.random() * 256)};
         
---
+++ script.test.js
@@ -0,0 +1,51 @@
+import {
+    generateRandomColor,
+    functionalFilterNegativeNumbers,
+    filterNegativeNumbers,
+    functionalMapNumbersIntoStrings,
+    mapNumbersIntoStrings,
+    printType,
+    isPalindrome,
+    printOutPersonAge,
+        
+} from './script.js';
+describe('generateRandomColor',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('functionalFilterNegativeNumbers',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('filterNegativeNumber',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('functionalMapNumbersIntoStrings',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('mapNumbersIntoStrings',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('printType',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('isPalindrome',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
+describe('printOutPersonAge',()=>{
+    it("should work",() =>{
+        expect(1).toBe(1);
+     });
+});
